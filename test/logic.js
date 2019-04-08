557d57dea90788104412a8ebba9b8963993d5465
Carles <carles.sanchez@namshi.com>
2019-04-08 06:44:26 +0400

    Added new tools, fixed coverage

--- test/logic.js
+++ test/logic.js
@@ -1,19 +1,36 @@
 const assert = require('assert');
-const {either} = require('../logic');
+const { either, not, identity } = require('../logic');
 
 describe('Logic', function() {
   describe('#either()', function() {
-    it('should return the result of function if there is no exception', function(){
-        let fn = either(JSON.parse,0);
-        assert.deepEqual(fn('{"a":3}'),{a:3});
+    it('should return the result of function if there is no exception', function() {
+      let fn = either(JSON.parse, 0);
+      assert.deepEqual(fn('{"a":3}'), { a: 3 });
     });
-    it('should return the default value if there is exception', function(){
-        let fn = either(JSON.parse,0);
-        assert.deepEqual(fn({}),0);
+    it('should return the default value if there is exception', function() {
+      let fn = either(JSON.parse, 0);
+      assert.deepEqual(fn({}), 0);
     });
-    it('should return the default value from a function if there is exception', function(){
-        let fn = either(JSON.parse,x => 0);
-        assert.deepEqual(fn({}),0);
+    it('should return the default value from a function if there is exception', function() {
+      let fn = either(JSON.parse, x => 0);
+      assert.deepEqual(fn({}), 0);
+    });
+  });
+  describe('not()', function() {
+    it('should negate the value passed if identity provided', function() {
+      assert.equal(not(identity)(true), false);
+    });
+    it('should return the opposite value given by the function', function() {
+      assert.equal(not(x => x.length > 2)(''), true);
+      assert.equal(not(x => x.length > 2)('xxx'), false);
+    });
+  });
+  describe('identity()', function() {
+    it('should return the exact input passed', function() {
+      assert.equal(identity(3), 3);
+      assert.equal(identity(''), '');
+      assert.equal(identity(null), null);
+      assert.equal(identity(), undefined);
     });
   });
 });
